5b787a2eb3ff938735344e1629a5a7692fe7393e
rawniz <dariuszghoshal@gmail.com>
2017-01-12 11:13:52 +0000

    This is not the default index page

---
+++ app/controllers/task_one_index_controller.rb
@@ -0,0 +1,6 @@
+class TaskOneIndexController < ApplicationController
+
+  def index
+    render html: "This is not the default index page"
+  end
+end
---
+++ app/helpers/task_one_index_helper.rb
@@ -0,0 +1,2 @@
+module TaskOneIndexHelper
+end
