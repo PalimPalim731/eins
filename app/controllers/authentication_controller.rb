66ed0438dbbeab5743573d171b96f42d1ecedc29
rawniz <dariuszghoshal@gmail.com>
2017-01-25 17:09:20 +0000

    Login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,34 @@
 class AuthenticationController < ApplicationController
-  def login
-  end
+def new
+  @user = User.new
+end
 
-  def signup
+def create
+  @user = User.new(params[:user])
+  if @user.save
+    redirect_to root_url, :notice => "Signed up!"
+  else
+     render template: "authentication/signup"
   end
+end
+
+def new1
+end
+
+def authenticate
+  user = User.authenticate(params[:email], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Logged in!"
+  else
+    flash.now.alert = "Invalid email or password"
+    render "authentication/login"
+  end
+end
+
+def logout
+  session[:user_id] = nil
+  redirect_to root_url, :notice => "Logged out!"
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116161858) do
+ActiveRecord::Schema.define(version: 20170123173442) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password"
+    t.string   "hashed_password"
+    t.integer  "role"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
